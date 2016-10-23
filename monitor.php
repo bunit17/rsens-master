3919f0b180923a5df25c1cf70b987382f007a83c
bunit17 <gitcommits@mattburnett.co.uk>
2016-10-23 20:00:29 +0100

    Improved error handaling

--- monitor.php
+++ monitor.php
@@ -1,15 +1,23 @@
 <?php
 require("database_conc.inc");
 
-$stmt = $link->prepare("SELECT plug_raw.decibel FROM plug_raw ORDER BY plug_raw.id DESC LIMIT 1");
-$stmt->execute();
+if (!($stmt = $link->prepare("SELECT plug_raw.decibel FROM plug_raw ORDER BY plug_raw.id DESC LIMIT 1"))){
+		header('HTTP/1.1 500 database prepare error: '.$mysqli->errno, TRUE, 500);
+	}
+if (!$stmt->execute()){
+	header('HTTP/1.1 500 database execute error: '.$stmt->errno, TRUE, 500);
+}
 $stmt->bind_result($plug);
 	while ($stmt->fetch()) {
 		$json['plug'] = $plug;
 	}
 
-$stmt = $link->prepare("SELECT tub_raw.decibel FROM tub_raw ORDER BY tub_raw.id DESC LIMIT 1");
-$stmt->execute();
+if (!($stmt = $link->prepare("SELECT tub_raw.decibel FROM tub_raw ORDER BY tub_raw.id DESC LIMIT 1"))){
+		header('HTTP/1.1 500 database prepare error: '.$mysqli->errno, TRUE, 500);
+	}
+if (!$stmt->execute()){
+	header('HTTP/1.1 500 database execute error: '.$stmt->errno, TRUE, 500);
+}
 $stmt->bind_result($tub);
 	while ($stmt->fetch()) {
 		$json['tub'] = $tub;
@@ -19,5 +27,6 @@ $stmt->bind_result($tub);
 echo json_encode($json);
 
 mysqli_close($link);
+
 
 ?>
--- monitor_adverage.php
+++ monitor_adverage.php
@@ -3,8 +3,12 @@
 //SELECT * FROM (SELECT AVG(plug_raw.decibel) AS plug_adverage, MAX(plug_raw.decibel) AS plug_peak, AVG(tub_raw.decibel) AS tub_adverage, MAX(tub_raw.decibel) AS tub_peak from plug_raw, tub_raw) AS tmp 
 //WHERE EXISTS (SELECT id FROM tub_raw) AND EXISTS (SELECT id FROM plug_raw)
 require("database_conc.inc");
-$stmt = $link->prepare("SELECT *, ((tmp2.plug_adverage + tmp.live_plug_adverage)/2) as uptodate_plug_adverage, ((tmp2.tub_adverage + tmp.live_tub_adverage)/2) as uptodate_tub_adverage FROM (SELECT AVG(plug_raw.decibel) AS live_plug_adverage, AVG(tub_raw.decibel) AS live_tub_adverage from plug_raw, tub_raw) AS tmp, (SELECT plug_adverage, tub_adverage, (plug_adverage+plug_sd+plug_sd) as plug_peak, (tub_adverage+tub_sd+tub_sd) as tub_peak FROM adverage_data ORDER BY timestamp DESC LIMIT 1) as tmp2 WHERE EXISTS (SELECT id FROM tub_raw) AND EXISTS (SELECT id FROM plug_raw)");
-$stmt->execute();
+if (!($stmt = $link->prepare("SELECT *, ((tmp2.plug_adverage + tmp.live_plug_adverage)/2) as uptodate_plug_adverage, ((tmp2.tub_adverage + tmp.live_tub_adverage)/2) as uptodate_tub_adverage FROM (SELECT AVG(plug_raw.decibel) AS live_plug_adverage, AVG(tub_raw.decibel) AS live_tub_adverage from plug_raw, tub_raw) AS tmp, (SELECT plug_adverage, tub_adverage, (plug_adverage+plug_sd+plug_sd) as plug_peak, (tub_adverage+tub_sd+tub_sd) as tub_peak FROM adverage_data ORDER BY timestamp DESC LIMIT 1) as tmp2 WHERE EXISTS (SELECT id FROM tub_raw) AND EXISTS (SELECT id FROM plug_raw)"))){
+		header('HTTP/1.1 500 database prepare error: '.$mysqli->errno, TRUE, 500);
+	}
+if (!$stmt->execute()){
+	header('HTTP/1.1 500 database execute error: '.$stmt->errno, TRUE, 500);
+}
 $stmt->bind_result($live_plug_adverage, $live_tub_adverage, $plug_adverage, $tub_adverage, $plug_peak, $tub_peak, $uptodate_plug_adverage, $uptodate_tub_adverage);
 	while ($stmt->fetch()) {
 		$json['plug_adverage'] = $uptodate_plug_adverage;
