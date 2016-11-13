3292cd4b5956348137658bd84d3bfecbdcf737ba
Nick Bartley <github@nb221.com>
2016-11-13 18:32:59 +0000

    Modified grid

--- index.php
+++ index.php
@@ -71,13 +71,13 @@ body {background-color:black;}
 			<div id="tub_adverage" class="col-md-3 col-xs-6 guage sub_guage"></div>
 			<div id="tub_peak" class="col-md-3 col-xs-6 guage sub_guage"></div>
 		</div>
-		<div class="row info">
-			<div class="col-md-3"><h4><a href="history.php" class="today link">View todays data</a></h4><diV>
-			<div class="col-md-3"><h4><a href="history.php?date=<?php echo date('d-m-Y', time() - 60 * 60 * 24); ?>" class="yesterday link">View yesterdays data</a></h4></div>
-			<div class="col-md-3"><h4><a href="history.php" class="historic link">View historic data</a></h4></div>
+		<div class="row">
+			<div class="col-md-3 info"><h4><a href="history.php" class="today link">View todays data</a></h4><diV>
+			<div class="col-md-3 info"><h4><a href="history.php?date=<?php echo date('d-m-Y', time() - 60 * 60 * 24); ?>" class="yesterday link">View yesterdays data</a></h4></div>
+			<div class="col-md-3 info"><h4><a href="history.php" class="historic link">View historic data</a></h4></div>
 		</div>
-		<div class="row info">
-			<div class="col-md-12">
+		<div class="row">
+			<div class="col-md-12 info">
 				<h4>If there is an error or problem with this serivce please take a picture of the screen and email rsens@nb221.com</h4>
 			</div>
 		</div>
