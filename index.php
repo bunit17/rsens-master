6dbb8c4b518b79ac48d13ce47180754b39b13a07
Nick Bartley <github@nb221.com>
2016-11-13 18:36:28 +0000

    fix div error

--- index.php
+++ index.php
@@ -72,7 +72,7 @@ body {background-color:black;}
 			<div id="tub_peak" class="col-md-3 col-xs-6 guage sub_guage"></div>
 		</div>
 		<div class="row">
-			<div class="col-md-3 info"><a href="history.php" class="today link">View todays data</a><diV>
+			<div class="col-md-3 info"><a href="history.php" class="today link">View todays data</a></div>
 			<div class="col-md-3 info"><a href="history.php?date=<?php echo date('d-m-Y', time() - 60 * 60 * 24); ?>" class="yesterday link">View yesterdays data</a></div>
 			<div class="col-md-3 info"><a href="history.php" class="historic link">View historic data</a></div>
 		</div>
