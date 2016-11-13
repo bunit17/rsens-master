8cadf5031559efb4ba4e5b43ab32a3404c6584e4
bunit17 <gitcommits@mattburnett.co.uk>
2016-11-13 13:33:41 +0000

    Rolling 5 Minitue advrage

--- calculate_adverage.php
+++ calculate_adverage.php
@@ -1,15 +1,17 @@
 <?php
 require("database_conc.inc");
+$stmt2 = $link->prepare("DELETE FROM `tub_raw` WHERE `timestamp` < ADDDATE(NOW(), INTERVAL -5 MINUTE)");
+$stmt2->execute();
+$stmt3 = $link->prepare("DELETE FROM `plug_raw` WHERE `timestamp` < ADDDATE(NOW(), INTERVAL -5 MINUTE");
+$stmt3->execute();
+
 $stmt = $link->prepare("INSERT INTO adverage_data (plug_adverage, plug_peak, plug_sd, tub_adverage, tub_peak, tub_sd) 
 SELECT * FROM (SELECT AVG(plug_raw.decibel) AS plug_adverage, MAX(plug_raw.decibel) AS plug_peak, STD(plug_raw.decibel) AS plug_sd, AVG(tub_raw.decibel) AS tub_adverage, MAX(tub_raw.decibel) AS tub_peak, STD(tub_raw.decibel) AS tub_sd from plug_raw, tub_raw) AS tmp 
 WHERE EXISTS (SELECT id FROM tub_raw) AND EXISTS (SELECT id FROM plug_raw)");
 $stmt->execute();
 
 echo "".$stmt->affected_rows." rows inserted";
-$stmt2 = $link->prepare("truncate tub_raw");
-$stmt2->execute();
-$stmt3 = $link->prepare("truncate plug_raw");
-$stmt3->execute();
+
 
 mysqli_close($link);
  
--- monitor_adverage.php
+++ monitor_adverage.php
@@ -3,18 +3,18 @@
 //SELECT * FROM (SELECT AVG(plug_raw.decibel) AS plug_adverage, MAX(plug_raw.decibel) AS plug_peak, AVG(tub_raw.decibel) AS tub_adverage, MAX(tub_raw.decibel) AS tub_peak from plug_raw, tub_raw) AS tmp 
 //WHERE EXISTS (SELECT id FROM tub_raw) AND EXISTS (SELECT id FROM plug_raw)
 require("database_conc.inc");
-if (!($stmt = $link->prepare("SELECT *, ((tmp2.plug_adverage + tmp.live_plug_adverage)/2) as uptodate_plug_adverage, ((tmp2.tub_adverage + tmp.live_tub_adverage)/2) as uptodate_tub_adverage FROM (SELECT AVG(plug_raw.decibel) AS live_plug_adverage, AVG(tub_raw.decibel) AS live_tub_adverage from plug_raw, tub_raw) AS tmp, (SELECT plug_adverage, tub_adverage, (plug_adverage+plug_sd+plug_sd) as plug_peak, (tub_adverage+tub_sd+tub_sd) as tub_peak FROM adverage_data ORDER BY timestamp DESC LIMIT 1) as tmp2 WHERE EXISTS (SELECT id FROM tub_raw) AND EXISTS (SELECT id FROM plug_raw)"))){
+if (!($stmt = $link->prepare("SELECT * FROM (SELECT AVG(plug_raw.decibel) AS plug_adverage, MAX(plug_raw.decibel) AS plug_peak, STD(plug_raw.decibel) AS plug_sd, AVG(tub_raw.decibel) AS tub_adverage, MAX(tub_raw.decibel) AS tub_peak, STD(tub_raw.decibel) AS tub_sd from plug_raw, tub_raw WHERE plug_raw.timestamp > ADDDATE(NOW(), INTERVAL -5 MINUTE) AND tub_raw.timestamp > ADDDATE(NOW(), INTERVAL -5 MINUTE)) AS tmp WHERE EXISTS (SELECT id FROM tub_raw) AND EXISTS (SELECT id FROM plug_raw)"))){
 		header('HTTP/1.1 500 database prepare error: '.$mysqli->errno, TRUE, 500);
 	}
 if (!$stmt->execute()){
 	header('HTTP/1.1 500 database execute error: '.$stmt->errno, TRUE, 500);
 }
-$stmt->bind_result($live_plug_adverage, $live_tub_adverage, $plug_adverage, $tub_adverage, $plug_peak, $tub_peak, $uptodate_plug_adverage, $uptodate_tub_adverage);
+$stmt->bind_result($plug_adverage, $plug_peak, $plug_sd, $tub_adverage, $tub_peak, $tub_sd);
 	while ($stmt->fetch()) {
-		$json['plug_adverage'] = $uptodate_plug_adverage;
-		$json['plug_peak'] = $plug_peak;
-		$json['tub_adverage'] = $uptodate_tub_adverage;
-		$json['tub_peak'] = $tub_peak;
+		$json['plug_adverage'] = $plug_adverage;
+		$json['plug_peak'] = $plug_adverage+$plug_sd+$plug_sd;
+		$json['tub_adverage'] = $tub_adverage;
+		$json['tub_peak'] = $tub_adverage+$tub_sd+$tub_sd;
 	}
 
 echo json_encode($json);
