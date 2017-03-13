68e524d0df76dfb2abba0946fe8fd346508b2d40
Nick Bartley <github@nb221.com>
2017-03-13 19:01:23 +0000

    modified json

--- charting.php
+++ charting.php
@@ -1,22 +1,17 @@
 <?php
 require("database_conc.inc");
 
-//if (!($stmt = $link->prepare("SELECT * FROM (SELECT  MIN(timestamp) as timestamp, adverage_data.plug_adverage, adverage_data.tub_adverage, MIN(id) as id FROM adverage_data WHERE HOUR(timestamp) = 4 AND MINUTE(timestamp) = 45 GROUP BY DATE(timestamp), DAY(timestamp) ORDER BY id DESC LIMIT 300) sub ORDER BY timestamp ASC"))){
-//		header('HTTP/1.1 500 database prepare error: '.$mysqli->errno, TRUE, 500);
-if (!($stmt = $link->prepare("SELECT plug_raw.decibel FROM plug_raw ORDER BY plug_raw.id DESC LIMIT 10"))){
-		header('HTTP/1.1 500 database prepare error: '.$mysqli->errno, TRUE, 500);
-	}
-if (!$stmt->execute()){
-	header('HTTP/1.1 500 database execute error: '.$stmt->errno, TRUE, 500);
+$sql = "SELECT * FROM (SELECT MIN(timestamp) as timestamp, adverage_data.plug_adverage, adverage_data.tub_adverage, MIN(id) as id FROM adverage_data WHERE HOUR(timestamp) = 4 AND MINUTE(timestamp) = 45 GROUP BY DATE(timestamp), DAY(timestamp) ORDER BY id DESC LIMIT 300) sub ORDER BY timestamp ASC";
+
+$result = mysqli_query($link, $sql) or die("Error in Selecting " . mysqli_error($link));
+
+$tempData = array();
+while($row =mysqli_fetch_assoc($result))
+{
+	$tempData[] = $row;
 }
 
-$sth = mysqli_query("SELECT * FROM (SELECT MIN(timestamp) as timestamp, adverage_data.plug_adverage, adverage_data.tub_adverage, MIN(id) as id FROM adverage_data WHERE HOUR(timestamp) = 4 AND MINUTE(timestamp) = 45 GROUP BY DATE(timestamp), DAY(timestamp) ORDER BY id DESC LIMIT 300) sub ORDER BY timestamp ASC");
-
-while($r = mysqli_fetch_assoc($sth)) {
-    $rows[] = $r;
-}
-
-echo json_encode($rows);
+echo json_encode($tempData);
 
 mysqli_close($link);
 
