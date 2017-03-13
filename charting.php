8d181584fc2cfefa6356a3f6bc8f799911aeba19
Nick Bartley <github@nb221.com>
2017-03-13 18:41:16 +0000

    debugging myqsl

--- charting.php
+++ charting.php
@@ -1,7 +1,9 @@
 <?php
 require("database_conc.inc");
 
-if (!($stmt = $link->prepare("SELECT * FROM (SELECT  MIN(timestamp) as timestamp, adverage_data.plug_adverage, adverage_data.tub_adverage, MIN(id) as id FROM adverage_data WHERE HOUR(timestamp) = 4 AND MINUTE(timestamp) = 45 GROUP BY DATE(timestamp), DAY(timestamp) ORDER BY id DESC LIMIT 300) sub ORDER BY timestamp ASC"))){
+//if (!($stmt = $link->prepare("SELECT * FROM (SELECT  MIN(timestamp) as timestamp, adverage_data.plug_adverage, adverage_data.tub_adverage, MIN(id) as id FROM adverage_data WHERE HOUR(timestamp) = 4 AND MINUTE(timestamp) = 45 GROUP BY DATE(timestamp), DAY(timestamp) ORDER BY id DESC LIMIT 300) sub ORDER BY timestamp ASC"))){
+//		header('HTTP/1.1 500 database prepare error: '.$mysqli->errno, TRUE, 500);
+if (!($stmt = $link->prepare("SELECT plug_raw.decibel FROM plug_raw ORDER BY plug_raw.id DESC LIMIT 1"))){
 		header('HTTP/1.1 500 database prepare error: '.$mysqli->errno, TRUE, 500);
 	}
 if (!$stmt->execute()){
