2675be71b773b62d1fd93354ce5e5c64b00df9c3
Nick Bartley <github@nb221.com>
2017-03-19 13:30:08 +0000

    modified status

--- sensorStatus.php
+++ sensorStatus.php
@@ -4,22 +4,24 @@ require("database_conc.inc");
 $sql_tub = "SELECT tub_raw.timestamp FROM tub_raw WHERE tub_raw.timestamp > (NOW() - INTERVAL 1 SECOND) LIMIT 1";
 $sql_plug = "SELECT plug_raw.timestamp FROM plug_raw WHERE plug_raw.timestamp > (NOW() - INTERVAL 1 SECOND) LIMIT 0";
 
-//$result_tub = mysqli_query($link, $sql_tub) or die("Error in Selecting " . mysqli_error($link));
-//$result_plug = mysqli_query($link, $sql_plug) or die("Error in Selecting " . mysqli_error($link));
+$result_tub = mysqli_query($link, $sql_tub) or die("Error in Selecting " . mysqli_error($link));
+$result_plug = mysqli_query($link, $sql_plug) or die("Error in Selecting " . mysqli_error($link));
 
 $tempData = array();
 
 if ($result_tub){
-	$tempData["Tub"] = True
+	$tub = True
 }else{
-	$tempData["Tub"] = False
+	$tub = False
 }
 
 if ($result_plug){
-	$tempData["Plug"] = True
+	$plug = True
 }else{
-	$tempData["Plug"] = False
+	$plug = False
 }
+
+$tempData = ['tub' => $tub, 'plug' => $plug]
 
 echo json_encode($tempData);
 
