5acf3970d510efeb051c23d0269c5303a2d313c8
Nick Bartley <github@nb221.com>
2017-03-19 13:23:40 +0000

    Modified sensorStatus json

--- sensorStatus.php
+++ sensorStatus.php
@@ -1,16 +1,24 @@
 <?php
 require("database_conc.inc");
 
-$sql_tub = "SELECT * FROM `tub_raw` ORDER BY `timestamp` DESC WHERE timestamp > (now() - INTERVAL 1 SECOND)";
-$sql_plug = "SELECT * FROM `plug_raw` ORDER BY `timestamp` DESC WHERE timestamp > (now() - INTERVAL 1 SECOND)";
+$sql_tub = "SELECT tub_raw.timestamp FROM tub_raw WHERE tub_raw.timestamp > (NOW() - INTERVAL 1 SECOND) LIMIT 1";
+$sql_plug = "SELECT plug_raw.timestamp FROM plug_raw WHERE plug_raw.timestamp > (NOW() - INTERVAL 1 SECOND) LIMIT 0";
 
 $result_tub = mysqli_query($link, $sql_tub) or die("Error in Selecting " . mysqli_error($link));
 $result_plug = mysqli_query($link, $sql_plug) or die("Error in Selecting " . mysqli_error($link));
 
 $tempData = array();
-while($row =mysqli_fetch_assoc($result_tub))
-{
-	$tempData[] = $row;
+
+if ($result_tub){
+	tempData["Tub"] = True
+}else{
+	tempData["Tub"] = False
+}
+
+if ($result_plug){
+	tempData["Plug"] = True
+}else{
+	tempData["Plug"] = False
 }
 
 echo json_encode($tempData);
