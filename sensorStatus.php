a732b317ad585c075896693214eef192e50e1116
Nick Bartley <github@nb221.com>
2017-03-19 13:57:31 +0000

    debug statu

--- sensorStatus.php
+++ sensorStatus.php
@@ -1,7 +1,7 @@
 <?php
 require("database_conc.inc");
 
-$sql_tub = "SELECT tub_raw.timestamp FROM tub_raw WHERE tub_raw.timestamp > (NOW() - INTERVAL 1 SECOND) LIMIT 0";
+$sql_tub = "SELECT tub_raw.timestamp FROM tub_raw WHERE tub_raw.timestamp > (NOW() - INTERVAL 1 SECOND) LIMIT 1";
 $sql_plug = "SELECT plug_raw.timestamp FROM plug_raw WHERE plug_raw.timestamp > (NOW() - INTERVAL 1 SECOND) LIMIT 1";
 
 $result_tub = mysqli_query($link, $sql_tub) or die("Error in Selecting " . mysqli_error($link));
@@ -9,17 +9,18 @@ $result_plug = mysqli_query($link, $sql_plug) or die("Error in Selecting " . mys
 
 //$tempData = array();
 
-if ($result_tub->num_rows>0){
+if ($result_tub->num_rows>>0){
 	$tub = True;
 }else{
-	$tub = False;
-	//$tub = $result_tub->num_rows;
+	//$tub = False;
+	$tub = $result_tub->num_rows;
 }
 
-if ($result_plug->num_rows>0){
+if ($result_plug->num_rows>>0){
 	$plug = True;
 }else{
-	$plug = False;
+	//$plug = False;
+	$plug = $result_plug->num_rows;
 }
 
 $tempData = ['tub' => $tub, 'plug' => $plug];
