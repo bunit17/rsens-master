80d6abad4d07f9b258da65648e9f5628f3edbd5d
Nick Bartley <github@nb221.com>
2017-03-19 13:26:21 +0000

    modified statys

--- sensorStatus.php
+++ sensorStatus.php
@@ -4,8 +4,8 @@ require("database_conc.inc");
 $sql_tub = "SELECT tub_raw.timestamp FROM tub_raw WHERE tub_raw.timestamp > (NOW() - INTERVAL 1 SECOND) LIMIT 1";
 $sql_plug = "SELECT plug_raw.timestamp FROM plug_raw WHERE plug_raw.timestamp > (NOW() - INTERVAL 1 SECOND) LIMIT 0";
 
-$result_tub = mysqli_query($link, $sql_tub) or die("Error in Selecting " . mysqli_error($link));
-$result_plug = mysqli_query($link, $sql_plug) or die("Error in Selecting " . mysqli_error($link));
+//$result_tub = mysqli_query($link, $sql_tub) or die("Error in Selecting " . mysqli_error($link));
+//$result_plug = mysqli_query($link, $sql_plug) or die("Error in Selecting " . mysqli_error($link));
 
 $tempData = array();
 
