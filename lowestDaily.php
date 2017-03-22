e66cb3497961a4de9a9c4aa33dde9ba94554de12
Nick Bartley <github@nb221.com>
2017-03-22 10:10:19 +0000

    Added tub to olowest dauly

--- lowestDaily.php
+++ lowestDaily.php
@@ -1,15 +1,27 @@
 <?php
 require("database_conc.inc");
 
-$sql = "SELECT * FROM (SELECT MIN(timestamp) as timestamp, MIN(plug_adverage) as plug_adverage FROM adverage_data GROUP BY DATE(timestamp), DAY(timestamp) ORDER BY id DESC LIMIT 300) sub ORDER BY timestamp ASC";
+$sql1 = "SELECT * FROM (SELECT MIN(timestamp) as timestamp, MIN(plug_adverage) as plug_adverage FROM adverage_data GROUP BY DATE(timestamp), DAY(timestamp) ORDER BY id DESC LIMIT 300) sub ORDER BY timestamp ASC";
+$result1 = mysqli_query($link, $sql1) or die("Error in Selecting " . mysqli_error($link));
 
-$result = mysqli_query($link, $sql) or die("Error in Selecting " . mysqli_error($link));
+$sql2 = "SELECT * FROM (SELECT MIN(timestamp) as timestamp, MIN(tub_adverage) as tub_adverage FROM adverage_data GROUP BY DATE(timestamp), DAY(timestamp) ORDER BY id DESC LIMIT 300) sub ORDER BY timestamp ASC";
+$result2 = mysqli_query($link, $sql2) or die("Error in Selecting " . mysqli_error($link));
 
 $tempData = array();
-while($row = mysqli_fetch_assoc($result))
+$tempData1 = array();
+$tempData2 = array();
+
+while($row = mysqli_fetch_assoc($result1))
 {
-	$tempData[] = $row;
+	$tempData1[] = $row;
 }
+
+while($row = mysqli_fetch_assoc($result2))
+{
+	$tempData2[] = $row;
+}
+
+$tempData = ['tub' => $tempData2, 'plug' => $tempData1];
 
 echo json_encode($tempData);
 
