bc9610773e278d9345dba13bf221449509760308
Nick Bartley <github@nb221.com>
2017-03-22 10:05:58 +0000

    updated lowest daily

--- lowestDaily.php
+++ lowestDaily.php
@@ -1,7 +1,7 @@
 <?php
 require("database_conc.inc");
 
-$sql = "SELECT * FROM (SELECT MIN(timestamp) as timestamp, adverage_data.plug_adverage, MIN(plug_adverage) as plug_adverage FROM adverage_data GROUP BY DATE(timestamp), DAY(timestamp) ORDER BY id DESC LIMIT 300) sub ORDER BY timestamp ASC";
+$sql = "SELECT * FROM (SELECT MIN(timestamp) as timestamp, MIN(plug_adverage) as plug_adverage FROM adverage_data GROUP BY DATE(timestamp), DAY(timestamp) ORDER BY id DESC LIMIT 300) sub ORDER BY timestamp ASC";
 
 $result = mysqli_query($link, $sql) or die("Error in Selecting " . mysqli_error($link));
 
