46319c613e421f17017ad68fa303ad702b967b50
bunit17 <gitcommits@mattburnett.co.uk>
2017-03-26 12:29:28 +0100

    Added timezone conversion to charts

--- highPeak.php
+++ highPeak.php
@@ -1,7 +1,7 @@
 <?php
 require("database_conc.inc");
 
-$sql = "SELECT * FROM (SELECT MIN(timestamp) as timestamp, adverage_data.plug_peak, adverage_data.tub_peak, MIN(id) as id FROM adverage_data WHERE HOUR(timestamp) = 1 AND MINUTE(timestamp) = 00 GROUP BY DATE(timestamp), DAY(timestamp) ORDER BY id DESC LIMIT 300) sub ORDER BY timestamp ASC";
+$sql = "SELECT * FROM (SELECT MIN(CONVERT_TZ(timestamp,  'UTC',  'Europe/London' )) as timestamp, adverage_data.plug_peak, adverage_data.tub_peak, MIN(id) as id FROM adverage_data WHERE HOUR(timestamp) = 1 AND MINUTE(timestamp) = 00 GROUP BY DATE(timestamp), DAY(timestamp) ORDER BY id DESC LIMIT 300) sub ORDER BY timestamp ASC";
 
 $result = mysqli_query($link, $sql) or die("Error in Selecting " . mysqli_error($link));
 
--- lowestDaily.php
+++ lowestDaily.php
@@ -1,10 +1,10 @@
 <?php
 require("database_conc.inc");
 
-$sql1 = "SELECT * FROM (SELECT MIN(timestamp) as timestamp, MIN(plug_adverage) as plug_adverage FROM adverage_data GROUP BY DATE(timestamp), DAY(timestamp) ORDER BY id DESC LIMIT 300) sub ORDER BY timestamp ASC";
+$sql1 = "SELECT * FROM (SELECT MIN(CONVERT_TZ(timestamp,  'UTC',  'Europe/London' )) as timestamp, MIN(plug_adverage) as plug_adverage FROM adverage_data GROUP BY DATE(timestamp), DAY(timestamp) ORDER BY id DESC LIMIT 300) sub ORDER BY timestamp ASC";
 $result1 = mysqli_query($link, $sql1) or die("Error in Selecting " . mysqli_error($link));
 
-$sql2 = "SELECT * FROM (SELECT MIN(timestamp) as timestamp, MIN(tub_adverage) as tub_adverage FROM adverage_data GROUP BY DATE(timestamp), DAY(timestamp) ORDER BY id DESC LIMIT 300) sub ORDER BY timestamp ASC";
+$sql2 = "SELECT * FROM (SELECT MIN(CONVERT_TZ(timestamp,  'UTC',  'Europe/London' )) as timestamp, MIN(tub_adverage) as tub_adverage FROM adverage_data GROUP BY DATE(timestamp), DAY(timestamp) ORDER BY id DESC LIMIT 300) sub ORDER BY timestamp ASC";
 $result2 = mysqli_query($link, $sql2) or die("Error in Selecting " . mysqli_error($link));
 
 $tempData = array();
